aa579c174cf9ee9b1ad58ae3a784ba35ed84c058
Fernando Borretti <eudoxiahp@gmail.com>
2014-12-30 18:58:05 -0200

    Update to changes in the latest common-doc

--- src/vertex.lisp
+++ src/vertex.lisp
@@ -1,25 +1,25 @@
 (in-package :cl-user)
 (defpackage vertex
-  (:use :cl)
+  (:use :cl :common-doc.format)
   (:export :<vertex>)
   (:documentation "The main interface."))
 (in-package :vertex)
 
-(defclass <vertex> (common-doc:<format>)
+(defclass <vertex> (<format>)
   ()
   (:documentation "The VerTeX format."))
 
-(defmethod common-doc:parse-document ((vertex <vertex>)
-                                      (string string))
+(defmethod parse-document ((vertex <vertex>)
+                           (string string))
   "Return a VerTeX document parsed from a string."
   (vertex.parser:parse-string string))
 
-(defmethod common-doc:parse-document ((vertex <vertex>)
-                                      (pathname pathname))
+(defmethod parse-document ((vertex <vertex>)
+                           (pathname pathname))
   "Return a VerTeX document parsed from a file."
   (vertex.parser:parse-file pathname))
 
-(defmethod common-doc:emit-document ((vertex <vertex>)
-                                     (doc common-doc:<document>)
-                                     stream)
+(defmethod emit-document ((vertex <vertex>)
+                          (doc common-doc:<document>)
+                          stream)
   (error "Not implemented."))
